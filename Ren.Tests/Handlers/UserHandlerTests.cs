46669465cba80184c74bcc59700d6c2cebf1dff4
= <lucasilva108@gmail.com>
2019-11-18 17:50:56 -0300

    user handler tests

--- Ren.Tests/Handlers/UserHandlerTests.cs
+++ Ren.Tests/Handlers/UserHandlerTests.cs
@@ -1,5 +1,7 @@
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Ren.Domain.Commands.Inputs.Users;
+using Ren.Domain.Enums;
 using Ren.Domain.Handlers;
 using Ren.Domain.Repositories;
 
@@ -12,34 +14,53 @@ namespace Ren.Tests.Handlers
 
         [TestMethod]
         [TestCategory("Handlers")]
-        public void ShouldReturnInvalidWhenUserIsNull()
+        public void ShouldReturnInvalidWhenUserNameIsNull()
         {
-            // var command = new CreateUserCommand();
-            // command.FirstName = "";
-            // command.LastName = "";
-            // command.Document = "06828764374";
-            // command.Email = "teste@example.com";
-            // command.Password = "password";
-            // command.Phone = "";
-            // command.Image = "";
-            // var handler = new UserHandler(_userRepository);
-            // handler.Handle(command);
-            // Assert.AreEqual(true, handler.Invalid);
-            Assert.Fail();
+            var command = new CreateUserCommand();
+            command.FirstName = null;
+            command.LastName = null;
+            command.Document = "06828764374";
+            command.Email = "teste@example.com";
+            command.Password = "password";
+            command.Phone = "";
+            command.Image = "";
+            var handler = new UserHandler(_userRepository);
+            handler.Handle(command);
+            Assert.AreEqual(true, handler.Invalid);
         }
 
         [TestMethod]
         [TestCategory("Handlers")]
         public void ShouldReturnInvalidWhenCreateCommandIsInvalid()
         {
-            Assert.Fail();
+            var command = new CreateUserCommand();
+            command.FirstName = null;
+            command.LastName = null;
+            command.Document = "";
+            command.Email = "";
+            command.Password = "";
+            command.Phone = "";
+            command.Image = "";
+            var handler = new UserHandler(_userRepository);
+            handler.Handle(command);
+            Assert.AreNotEqual(true, handler.Valid);
         }
 
         [TestMethod]
         [TestCategory("Handlers")]
         public void ShoudlReturnInvalidWhenEditCommandIsInvalid()
         {
-            Assert.Fail();
+            var command = new EditUserCommand();
+            command.Id = Guid.NewGuid();
+            command.FirstName = "";
+            command.LastName = "";
+            command.Status = EUserStatus.Active;
+            command.Email = "email@example.com";
+            command.Phone = "";
+            command.Image = "";
+            var handler = new UserHandler(_userRepository);
+            handler.Handle(command);
+            Assert.AreEqual(false, handler.Valid);
         }
 
         [TestMethod]
